3008169fc22f65d7a38fc7fd3c0e362d2a945607
thomasmahut <76153096+thomasmahut@users.noreply.github.com>
2021-01-17 10:30:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-self.addEventListener('install', listener: evt=>{
+self.addEventListener('install', listener: evt=>){
 	console.log(evt);
-});
-self.addEventListener('activate', listener: evt=>{
+};
+self.addEventListener('activate', listener: evt=>){
 	console.log(evt);
-});
-self.addEventListener('fetch', evt=>{
+};
+self.addEventListener('fetch', evt=>){
     if (!navigator.onLine){
         evt.respondWith( new Response('pas de connexion internet'))
     }
     console.log(evt.request.url);
-})
+}
 
